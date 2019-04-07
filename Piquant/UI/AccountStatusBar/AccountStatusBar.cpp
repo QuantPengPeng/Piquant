46864c0aa914ea61b2910d265fc19b35cc150bdc
张鹏 <1983591408@qq.com>
2019-04-07 15:15:09 +0800

    AccountStatusBar

---
+++ Piquant/UI/AccountStatusBar/AccountStatusBar.cpp
@@ -0,0 +1,31 @@
+// AccountStatusBar.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "../App.h"
+#include "AccountStatusBar.h"
+
+
+// CAccountStatusBar
+
+IMPLEMENT_DYNAMIC(CAccountStatusBar, CMFCStatusBar)
+
+CAccountStatusBar::CAccountStatusBar()
+{
+	
+}
+
+CAccountStatusBar::~CAccountStatusBar()
+{
+	
+}
+
+
+BEGIN_MESSAGE_MAP(CAccountStatusBar, CMFCStatusBar)
+END_MESSAGE_MAP()
+
+
+
+// CAccountStatusBar message handlers
+
+
---
+++ Piquant/UI/AccountStatusBar/AccountStatusBar.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// CAccountStatusBar
+
+class CAccountStatusBar : public CMFCStatusBar
+{
+	DECLARE_DYNAMIC(CAccountStatusBar)
+
+public:
+	CAccountStatusBar();
+	virtual ~CAccountStatusBar();
+
+protected:
+	DECLARE_MESSAGE_MAP()
+
+};
+
+
